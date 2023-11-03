9d47277ba1bd104f433c9f9dbc5bdac7d5c03ee9
김소련 <kimsr9210@thekeytech.co.kr>
2023-11-03 13:57:02 +0900

    파일추가

---
+++ src/main/java/hello/core/member/Grade.java
@@ -0,0 +1,7 @@
+package hello.core.member;
+
+public enum Grade {
+    //회원에 대한 등급
+    BASIC,
+    VIP
+}
---
+++ src/main/java/hello/core/member/Member.java
@@ -0,0 +1,8 @@
+package hello.core.member;
+
+public class Member {
+    
+    //생성자
+    
+    //겟터셋터
+}
