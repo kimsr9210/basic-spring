7d6fb68549d1f4b9de8340f2c49afbcba6627b30
김소련 <kimsr9210@naver.com>
2023-11-06 11:06:46 +0900

    junit 테스트 코드

---
+++ src/test/java/hello/core/member/MemberServiseTest.java
@@ -0,0 +1,16 @@
+package hello.core.member;
+
+import org.junit.jupiter.api.Test;
+
+public class MemberServiseTest {
+
+    @Test
+    void join(){
+        //given 주어졌을때
+        //when 이렇게 했을대
+        //then 이렇게 된다 (검증)
+        
+        //똑같냐
+        //join한거랑 찾은거랑 똑같으면 초록색 불 뜸 틀리면 x가틈 맞으면
+    }
+}
