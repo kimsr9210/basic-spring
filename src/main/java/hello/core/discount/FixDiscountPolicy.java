93bae48b720d0223acd3ead575ba7536a5582b89
김소련 <kimsr9210@naver.com>
2023-11-09 16:54:09 +0900

    할인 정책 인터페이스 정액 할인 정책 구현체

---
+++ src/main/java/hello/core/discount/FixDiscountPolicy.java
@@ -0,0 +1,20 @@
+package hello.core.discount;
+
+import hello.core.member.Grade;
+import hello.core.member.Member;
+
+//정액 할인 정책 구현체
+public class FixDiscountPolicy implements DiscountPolicy{
+    
+    private int discountFixAmount = 1000; //1000원 할인
+    
+    @Override
+    public int discount(Member member, int price) {
+        //enum 타입은 == 을 써야함
+        if(member.getGrade() == Grade.VIP){ //VIP면 1000원 할인
+            return discountFixAmount;
+        } else {
+            return 0;
+        }
+    }
+}
